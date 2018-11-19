d9fbaa744879e7651ff53af09abe55d315fcdc7b
Kayla <keoliva@andrew.cmu.edu>
2018-11-19 11:47:14 -0500

    wrap is working

--- wrap.js
+++ wrap.js
@@ -1,4 +1,20 @@
 const wrap = (line, maxLen) => {
-  return line
+  if (line.length <= maxLen) return line;
+  line += ' ';
+  line = line.split('');
+  let fromIndex = maxLen;
+  const result = [];
+  let newLine;
+  let i = 0;
+
+  while (line.length !== 0) {// && i <= 5) {
+    //i++;
+    console.log(line);
+    newLine = line.splice(0, line.lastIndexOf(' ', maxLen) + 1)
+    console.log(newLine);
+    result.push(newLine.join('').trim());
+    console.log(result);
+  }
+  return result.join('\n');
 };
 module.exports = wrap;
--- wrap.spec.js
+++ wrap.spec.js
@@ -12,8 +12,21 @@ describe('wrap', () => {
   })
 
   it('Returns a string where no line is longer than column length', () => {
-    expect(wrap("hello world", 5).length < 5).to.equal(true)
+    expect(wrap("hello", 7).length <= 7).to.equal(true)
+  });
+  let argument;
+  let loret;
+  beforeEach(function() {
+    argument = "hello world";
+    loret = "Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.";
+  });
+
+
+  it('Checks no line is longer than the column number', () => {
+    expect(wrap(loret, 10).split('\n').every(line => line.length <= 10)).to.equal(true);
+    expect(wrap(argument, 5).split('\n').every(line => line.length <= 5)).to.equal(true);
   })
 
 });
+
 
